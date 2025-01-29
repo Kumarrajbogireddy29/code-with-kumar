73d7bfefa53963a41448d90391be1ee317389f98
Kumar Raj Bogireddy <kumarrajbogireddy29@gmail.com>
2025-01-29 20:31:30 +0530

    Add files via upload

---
+++ pattern11.py
@@ -0,0 +1,15 @@
+r=9
+n=r-1
+for i in range(r-1,-1,-1):
+    for j in range(n-i):
+        print(" ",end="")
+    for k in range(i*2+1):
+        print("*",end="")
+    print()
+for i in range(1,r):
+    for j in range(n-i):
+        print(" ",end="")
+    temp = i*2+1
+    for k in range(temp):
+        print("*",end="")
+    print()
