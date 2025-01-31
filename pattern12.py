cbd64d4223bdb1d8229eb9bf0f467ddfa44f785b
Kumar Raj Bogireddy <kumarrajbogireddy29@gmail.com>
2025-01-31 22:13:13 +0530

    Add files via upload

---
+++ pattern12.py
@@ -0,0 +1,10 @@
+r=4
+for i in range(r):
+    for j in range(r-i-1):
+       print(" ",end="")
+    for j in range(i*2+1):
+        if j==0 or j==i*2 or i==r-1:
+         print("*",end="")
+        else:
+         print(" ",end="")
+    print()
