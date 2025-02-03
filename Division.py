6264fd55693c7f0d5fbce3f8f31ffa60aefe30ba
Kumar Raj Bogireddy <kumarrajbogireddy29@gmail.com>
2025-02-03 23:56:36 +0530

    Add files via upload

---
+++ Division.py
@@ -0,0 +1,7 @@
+num1=int(input("Enter the First Number: "))
+num2=int(input("Enter the Second Number:"))
+if num2==0:
+    print("Error: Divison by zero not Allowed")
+else :
+    div_result=num1/num2
+    print( div_result)
