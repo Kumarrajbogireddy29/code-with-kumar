284c093e3132cda69f506515b4a271fca2cbf763
Kumar Raj Bogireddy <kumarrajbogireddy29@gmail.com>
2025-02-07 23:54:39 +0530

    Add files via upload

---
+++ swapnos.py
@@ -0,0 +1,7 @@
+a=int(input("Enter trhe value of a:"))
+b=int(input("Enter the value of b:"))
+print(f"The values of a and b is {a} , {b}")
+temp=a
+a=b
+b=temp
+print(f"Swapped values of a={a}, b={b}")
